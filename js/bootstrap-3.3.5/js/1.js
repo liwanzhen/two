a5eccce88cfd5cd454db430e96ef494f07d6567c
liwanzhan <2274036472@qq.com>
2016-12-01 16:02:36 +0800

    折叠菜单添加收缩展开图标。

--- js/bootstrap-3.3.5/js/1.js
+++ js/bootstrap-3.3.5/js/1.js
@@ -1,1 +1,19 @@
 "use strict"
+
+$(function(){
+	// 左侧菜单加收缩展开图标
+	var aMenu = $('#accordion > .panel-default');
+	var aMenuT = $('.panel-collapse');
+	$("#accordion .panel-default").each(function(i){
+		$(this).click(function(){
+			if($(this).find('.panel-collapse').hasClass('in')) {
+				$(aMenu[i]).find('span').removeClass('retract').addClass('spread');
+			} else {
+				for(var j = 0; j <= aMenuT.length; j ++){
+					$(aMenu[j]).find('span').removeClass('retract').addClass('spread');
+				}
+				$(aMenu[i]).find('span').removeClass('spread').addClass('retract');
+			}
+		});
+	});
+})
