47f9d303f06880f9312cc68587ea2e6d864ea721
liwanzhan <2274036472@qq.com>
2016-12-02 18:02:41 +0800

    折叠菜单调整。

--- js/bootstrap-3.3.5/js/1.js
+++ js/bootstrap-3.3.5/js/1.js
@@ -4,15 +4,15 @@ $(function(){
 	// 左侧菜单加收缩展开图标
 	var aMenu = $('#accordion > .panel-default');
 	var aMenuT = $('.panel-collapse');
-	$("#accordion .panel-default").each(function(i){
-		$(this).click(function(){
-			if($(this).find('.panel-collapse').hasClass('in')) {
-				$(aMenu[i]).find('span').removeClass('retract').addClass('spread');
+	$("#accordion .panel-default .panel-heading").each(function(i){
+		$(this).find('a').click(function(){
+			if($(this).parents('.panel-default').find('.panel-collapse').hasClass('in')) {
+				$(aMenu[i]).find('i.change').removeClass('icon-triangle-down').addClass('icon-triangle-right');
 			} else {
 				for(var j = 0; j <= aMenuT.length; j ++){
-					$(aMenu[j]).find('span').removeClass('retract').addClass('spread');
+					$(aMenu[j]).find('i.change').removeClass('icon-triangle-down').addClass('icon-triangle-right');
 				}
-				$(aMenu[i]).find('span').removeClass('spread').addClass('retract');
+				$(aMenu[i]).find('i.change').removeClass('icon-triangle-right').addClass('icon-triangle-down');
 			}
 		});
 	});
